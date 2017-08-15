9b7934f7c1e2b955f5a2aa482adfdb7701fee867
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-08-14 22:09:42 -0300

    Separate books: already read, reading, want read

--- src/app.js
+++ src/app.js
@@ -9,83 +9,78 @@ class App extends PureComponent {
   constructor () {
     super()
     this.state = {
-      books: null
+      alreadyRead: [],
+      reading: [],
+      wantRead: [],
     }
   }
 
   componentWillMount () {
-    // bookApi.fetchWantRead()
-    //   .then((response) => {
-    //     console.log('fetchWantRead() success');
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log('fetchWantRead() error');
-    //     console.log(error);
-    //   });
-
-    // bookApi.fetchAlreadyRead()
-    //   .then((response) => {
-    //     console.log('fetchAlreadyRead() success');
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log('fetchAlreadyRead() error');
-    //     console.log(error);
-    //   });
-
-    // bookApi.fetchReading()
-    //   .then((response) => {
-    //     console.log('fetchReading() success');
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     console.log('fetchReading() error');
-    //     console.log(error);
-    //   });
-
-    bookApi.fetchAll()
-      .then((response) => {
-        const books = {}
-        const wantRead = response[0].data.response
-        const alreadyRead = response[1].data.response
-        const reading = response[2].data.response
-        books.wantRead = wantRead
-        books.alreadyRead = alreadyRead
-        books.reading = reading
-        this.setState({ books: books })
-      })
+    this.fetchAlreadyRead();
+    this.fetchReading();
+    this.fetchWantRead();
   }
 
-  renderBooks = (books) => {
-    if (books) {
-      return books.reading.map((book, index) => {
-        const cover = book.edicao.capa_media
-        const title = book.edicao.titulo
-        // const subti  tle = book.edicao.subtitulo
-        const author = book.edicao.autor
-        // const publishingCompany = book.edicao.editora
-        // const link = book.edicao.url
-
-        return (
-          <article>
-            <h2>{title}</h2>
-            <p>{author}</p>
-            <img src={cover} />
-          </article>
-        )
-      })
-    }
-
-    return 'carregando...'
+  handleError = (error) => {
+    console.log(error);
   }
+
+  fetchAlreadyRead = () => {
+    bookApi.fetchAlreadyRead()
+      .then(response => this.setState({ alreadyRead: response.data.response }))
+      .catch(error => this.handleError(error))
+  }
+
+  fetchReading = () => {
+    bookApi.fetchReading()
+      .then(response => this.setState({ reading: response.data.response }))
+      .catch(error => this.handleError(error));
+  }
+
+  fetchWantRead = () => {
+    bookApi.fetchWantRead()
+      .then(response => this.setState({ wantRead: response.data.response }))
+      .catch(error => this.handleError(error));
+  }
+
+  renderBooks = (books = []) => (
+    books.map((book, index) => {
+      const cover = book.edicao.capa_media
+      const title = book.edicao.titulo
+      // const subtitle = book.edicao.subtitulo
+      const author = book.edicao.autor
+      // const publishingCompany = book.edicao.editora
+      // const link = book.edicao.url
+
+      return (
+        <article>
+          <h3>{title}</h3>
+          <p>{author}</p>
+          <img src={cover} />
+        </article>
+      )
+    })
+  )
 
   render () {
-    const { books } = this.state
+    const { alreadyRead, reading, wantRead} = this.state;
 
     return (
       <div>
-        {this.renderBooks(books)}
+        <section>
+          <h2>Lendo:</h2>
+          {this.renderBooks(reading)}
+        </section>
+
+        <section>
+          <h2>Lidos:</h2>
+          {this.renderBooks(alreadyRead)}
+        </section>
+
+        <section>
+          <h2>Quero ler:</h2>
+          {this.renderBooks(wantRead)}
+        </section>
       </div>
     )
   }
