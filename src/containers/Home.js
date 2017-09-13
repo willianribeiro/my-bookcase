def360b7cb17e21575d065379ec9ebddaa3d64fd
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-09-13 19:06:28 -0300

    Put app content inside Home.js

---
+++ src/containers/Home.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import bookApi from '../api/bookApi'
+
+class Home extends Component {
+  constructor () {
+    super()
+    this.state = {
+      alreadyRead: [],
+      reading: [],
+      wantRead: []
+    }
+  }
+
+  componentWillMount () {
+    this.fetchAlreadyRead()
+    this.fetchReading()
+    this.fetchWantRead()
+  }
+
+  fetchAlreadyRead = () => {
+    bookApi.fetchAlreadyRead()
+      .then(response => this.setState({ alreadyRead: response.data.response }))
+      .catch(error => this.handleError(error))
+  }
+
+  fetchReading = () => {
+    bookApi.fetchReading()
+      .then(response => this.setState({ reading: response.data.response }))
+      .catch(error => this.handleError(error))
+  }
+
+  fetchWantRead = () => {
+    bookApi.fetchWantRead()
+      .then(response => this.setState({ wantRead: response.data.response }))
+      .catch(error => this.handleError(error))
+  }
+
+  handleError = (error) => {
+    console.log(error)
+  }
+
+  renderBooks = (books = []) => (
+    books.map((book, index) => {
+      const cover = book.edicao.capa_media
+      const title = book.edicao.titulo
+      // const subtitle = book.edicao.subtitulo
+      const author = book.edicao.autor
+      // const publishingCompany = book.edicao.editora
+      // const link = book.edicao.url
+
+      return (
+        <article key={index}>
+          <h3>{title}</h3>
+          <p>{author}</p>
+          <img src={cover} />
+        </article>
+      )
+    })
+  )
+
+  render () {
+    const { alreadyRead, reading, wantRead } = this.state
+
+    return (
+      <div>
+        <section>
+          <h2>Lendo:</h2>
+          {this.renderBooks(reading)}
+        </section>
+
+        <section>
+          <h2>Lidos:</h2>
+          {this.renderBooks(alreadyRead)}
+        </section>
+
+        <section>
+          <h2>Quero ler:</h2>
+          {this.renderBooks(wantRead)}
+        </section>
+      </div>
+    )
+  }
+}
+
+export default Home
