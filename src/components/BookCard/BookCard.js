cfe3da3b5967db283a67a2ed4f06ca76a9d8f21e
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-09-27 14:39:47 -0300

    Change BookCard design

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -8,13 +8,22 @@ const BookCard = ({
 }) => {
   return (
     <div className='c-book-card'>
-      <div className='c-book-card__cover' >
-        <img src={img} />
+      <div className='c-book-card__cover'>
+        <div className='c-book-card__book'>
+          <div className='c-book-card__book-front'>
+            <img src={img} className='c-book-card__img' />
+          </div>
+          <div className='c-book-card__book-back'></div>
+          <div className='c-book-card__book-side'></div>
+        </div>
       </div>
-
-      <div className='c-book-card__infos' >
-        <div className='c-book-card__title'>{title}</div>
-        <div className='c-book-card__author'>{author}</div>
+      <div>
+        <div className='c-book-card__title'>
+          {title}
+        </div>
+        <div className='c-book-card__author'>
+          {author}
+        </div>
       </div>
     </div>
   )
