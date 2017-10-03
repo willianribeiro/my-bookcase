8dfbe94817cf068622145978d146bac5b8206bed
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-10-03 16:34:55 -0300

    Refactor BookApi to receive page as parameter

--- src/api/BookApi.js
+++ src/api/BookApi.js
@@ -1,21 +1,39 @@
 import axios from 'axios'
 
-const proxy = 'https://cors-anywhere.herokuapp.com/'
-const wantRead = `${proxy}https://www.skoob.com.br/v1/bookcase/books/4355417/shelf_id:0/page:1/limit:36/`
-const alreadyRead = `${proxy}https://www.skoob.com.br/v1/bookcase/books/4355417/shelf_id:1/page:1/limit:36/`
-const reading = `${proxy}https://www.skoob.com.br/v1/bookcase/books/4355417/shelf_id:2/page:1/limit:36/`
-
 const api = {
-  fetchWantRead: () => axios.get(wantRead),
-  fetchAlreadyRead: () => axios.get(alreadyRead),
-  fetchReading: () => axios.get(reading),
-  fetchAll: () => (
-    axios.all([
-      axios.get(wantRead),
-      axios.get(alreadyRead),
-      axios.get(reading)
-    ])
-  )
+  fetchWantRead: fetchWantRead,
+  fetchAlreadyRead: fetchAlreadyRead,
+  fetchReading: fetchReading
+}
+
+// variables
+const proxy = 'https://cors-anywhere.herokuapp.com'
+const baseUrl = 'https://www.skoob.com.br/v1/bookcase/books'
+const userId = 4355417
+const limit = 20
+
+// public functions
+function fetchWantRead (page) {
+  const shelfId = 0
+  const wantRead = _mountUrl(shelfId, page)
+  return axios.get(wantRead)
+}
+
+function fetchAlreadyRead (page) {
+  const shelfId = 1
+  const alreadyRead = _mountUrl(shelfId, page)
+  return axios.get(alreadyRead)
+}
+
+function fetchReading (page) {
+  const shelfId = 2
+  const reading = _mountUrl(shelfId, page)
+  return axios.get(reading)
+}
+
+// private functions
+function _mountUrl(shelfId, page) {
+  return `${proxy}/${baseUrl}/${userId}/shelf_id:${shelfId}/page:${page}/limit:${limit}/`
 }
 
 export default api
--- src/api/MockBookApi.js
+++ src/api/MockBookApi.js
@@ -5,16 +5,9 @@ const alreadyRead = '/mock_data/books_already_read.json'
 const reading = '/mock_data/books_reading.json'
 
 const api = {
-  fetchWantRead: () => axios.get(wantRead),
-  fetchAlreadyRead: () => axios.get(alreadyRead),
-  fetchReading: () => axios.get(reading),
-  fetchAll: () => (
-    axios.all([
-      axios.get(wantRead),
-      axios.get(alreadyRead),
-      axios.get(reading)
-    ])
-  )
+  fetchWantRead: (page) => axios.get(wantRead),
+  fetchAlreadyRead: (page) => axios.get(alreadyRead),
+  fetchReading: (page) => axios.get(reading),
 }
 
 export default api
