5fe9fe46e20a859ffc39f1ab9d7f6ceaa45e12dc
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-10-03 16:35:18 -0300

    Use Paginator at Home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,52 +1,92 @@
-import React, {Component } from 'react'
+import React, { Component } from 'react'
 
 import BookApi from '../../api/BookApi'
 import { Tabs, Tab } from 'components/Tabs'
-import Bookshelf from 'components/Bookshelf'
 import AppHeader from 'components/AppHeader'
+import Bookshelf from 'components/Bookshelf'
+import Paginator from 'components/Paginator'
 
 class Home extends Component {
   constructor () {
     super()
     this.state = {
-      alreadyRead: [],
-      reading: [],
-      wantRead: [],
-      loadingAlreadyRead: true,
-      loadingReading: true,
-      loadingWantRead: true
+      alreadyRead: {
+        books: [],
+        pagination: {},
+        loading: false
+      },
+      reading: {
+        books: [],
+        pagination: {},
+        loading: false
+      },
+      wantRead: {
+        books: [],
+        pagination: {},
+        loading: false
+      },
+      alreadyReadInitialLoad: true,
+      readingInitialLoad: true,
+      wantReadInitialLoad: true
     }
   }
 
   componentWillMount () {
-    this.fetchAlreadyRead()
-    this.fetchReading()
-    this.fetchWantRead()
+    this.fetchAlreadyRead(1)
+    this.fetchReading(1)
+    this.fetchWantRead(1)
   }
 
-  fetchAlreadyRead = () => {
-    BookApi.fetchAlreadyRead()
+  fetchAlreadyRead = page => {
+    this.setState({ alreadyRead: { ...this.state.alreadyRead, loading: true } })
+
+    BookApi.fetchAlreadyRead(page)
       .then(response => {
-        this.setState({ alreadyRead: response.data.response })
-        this.setState({ loadingAlreadyRead: false })
+        const books = this.state.alreadyRead.books
+        const booksUpdated = books.concat(response.data.response)
+        const alreadyRead = {
+          books: booksUpdated,
+          pagination: response.data.paging,
+          loading: false
+        }
+        this.setState({ alreadyRead: alreadyRead })
+        this.setState({ alreadyReadInitialLoad: false })
       })
       .catch(error => this.handleError(error))
   }
 
-  fetchReading = () => {
-    BookApi.fetchReading()
+  fetchReading = page => {
+    this.setState({ reading: { ...this.state.reading, loading: true } })
+
+    BookApi.fetchReading(page)
       .then(response => {
-        this.setState({ reading: response.data.response })
-        this.setState({ loadingReading: false })
+        const books = this.state.reading.books
+        const booksUpdated = books.concat(response.data.response)
+        const reading = {
+          books: booksUpdated,
+          pagination: response.data.paging,
+          loading: false
+        }
+        this.setState({ reading: reading })
+        this.setState({ readingInitialLoad: false })
       })
       .catch(error => this.handleError(error))
   }
 
-  fetchWantRead = () => {
-    BookApi.fetchWantRead()
+  fetchWantRead = page => {
+    this.setState({ wantRead: { ...this.state.wantRead, loading: true } })
+
+    BookApi.fetchWantRead(page)
       .then(response => {
-        this.setState({ wantRead: response.data.response })
-        this.setState({ loadingWantRead: false })
+        const books = this.state.wantRead.books
+        const booksUpdated = books.concat(response.data.response)
+        const wantRead = {
+          books: booksUpdated,
+          pagination: response.data.paging,
+          loading: false
+        }
+        this.setState({ wantRead: wantRead })
+        this.setState({ wantReadInitialLoad: false })
       })
       .catch(error => this.handleError(error))
   }
@@ -60,9 +100,9 @@ class Home extends Component {
       alreadyRead,
       reading,
       wantRead,
-      loadingAlreadyRead,
-      loadingReading,
-      loadingWantRead
+      alreadyReadInitialLoad,
+      readingInitialLoad,
+      wantReadInitialLoad
     } = this.state
 
     return (
@@ -72,16 +112,43 @@ class Home extends Component {
         <div className='pg-home__content'>
           <Tabs initialTab={1} className='pg-custom-tabs'>
             <Tab title='Already read'>
-              {!loadingAlreadyRead && <Bookshelf books={alreadyRead} />}
-              {loadingAlreadyRead && <div className='c-loading'>Loading...</div>}
+              {!alreadyReadInitialLoad &&
+                <div>
+                  <Bookshelf books={alreadyRead.books} />
+                  <Paginator
+                    fetch={this.fetchAlreadyRead}
+                    pagination={alreadyRead.pagination}
+                    loading={alreadyRead.loading}
+                  />
+                </div>
+              }
+              {alreadyReadInitialLoad && <div className='c-loading'>Loading...</div>}
             </Tab>
             <Tab title='Reading'>
-              {!loadingReading && <Bookshelf books={reading} />}
-              {loadingReading && <div className='c-loading'>Loading...</div>}
+              {!readingInitialLoad &&
+                <div>
+                  <Bookshelf books={reading.books} />
+                  <Paginator
+                    fetch={this.fetchReading}
+                    pagination={reading.pagination}
+                    loading={reading.loading}
+                  />
+                </div>
+              }
+              {readingInitialLoad && <div className='c-loading'>Loading...</div>}
             </Tab>
             <Tab title='Want to read'>
-              {!loadingWantRead && <Bookshelf books={wantRead} />}
-              {loadingWantRead && <div className='c-loading'>Loading...</div>}
+              {!wantReadInitialLoad &&
+                <div>
+                  <Bookshelf books={wantRead.books} />
+                  <Paginator
+                    fetch={this.fetchWantRead}
+                    pagination={wantRead.pagination}
+                    loading={reading.loading}
+                  />
+                </div>
+              }
+              {wantReadInitialLoad && <div className='c-loading'>Loading...</div>}
             </Tab>
           </Tabs>
         </div>
